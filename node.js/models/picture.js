c2196d8a78fffabfdc04de0e25643ee6d8d8239f
woojune511 <geonju511@naver.com>
2020-07-17 22:59:40 +0900

    Update picture.js

--- node.js/models/picture.js
+++ node.js/models/picture.js
@@ -17,36 +17,36 @@ var picture = new Picture({
 
 
 
-// Create new todo document
-gallerySchema.statics.create = function (payload) {
-    // this === Model
-    const Picture = new this(payload);
-    // return Promise
-    return Picture.save();
-  };
+// // Create new todo document
+// gallerySchema.statics.create = function (payload) {
+//     // this === Model
+//     const Picture = new this(payload);
+//     // return Promise
+//     return Picture.save();
+//   };
   
-  // Find All
-  gallerySchema.statics.findAll = function () {
-    // return promise
-    // V4부터 exec() 필요없음
-    return this.find({});
-  };
+//   // Find All
+//   gallerySchema.statics.findAll = function () {
+//     // return promise
+//     // V4부터 exec() 필요없음
+//     return this.find({});
+//   };
   
-  // Find One by todoid
-  gallerySchema.statics.findOneByName = function (name) {
-    return this.findOne({ name });
-  };
+//   // Find One by todoid
+//   gallerySchema.statics.findOneByName = function (name) {
+//     return this.findOne({ name });
+//   };
   
-  // Update by todoid
-  gallerySchema.statics.updateByName = function (name, payload) {
-    // { new: true }: return the modified document rather than the original. defaults to false
-    return this.findOneAndUpdate({ name }, payload, { new: true });
-  };
+//   // Update by todoid
+//   gallerySchema.statics.updateByName = function (name, payload) {
+//     // { new: true }: return the modified document rather than the original. defaults to false
+//     return this.findOneAndUpdate({ name }, payload, { new: true });
+//   };
   
-  // Delete by todoid
-  gallerySchema.statics.deleteByName = function (name) {
-    return this.remove({ name });
-  };
+//   // Delete by todoid
+//   gallerySchema.statics.deleteByName = function (name) {
+//     return this.remove({ name });
+//   };
 
 
 // Create Model & Export
