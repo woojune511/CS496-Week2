78e0bff8aec4c12096fa25c3c3abb0ac75db78fe
woojune511 <geonju511@naver.com>
2020-07-17 16:03:21 +0900

    dbcon.js modified

--- node.js/dbcon.js
+++ node.js/dbcon.js
@@ -1,10 +1,14 @@
 var Client = require('mongodb').MongoClient;
 
-Client.connect('mongodb://localhost:27017/school', function(error, db){
+Client.connect('mongodb://localhost:27017/test', function(error, db){
     if(error) {
         console.log(error);
     } else {
         console.log("connected:"+db);
+        var adminDb = db.admin();
+        // List all the available databases
+        adminDb.listDatabases(function(err, result) {
+          console.log(result.databases);
         db.close();
     }
 });
