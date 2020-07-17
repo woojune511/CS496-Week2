b715c577a02a7fdccd56bdc940ef6de408c62ffd
woojune511 <geonju511@naver.com>
2020-07-18 01:01:39 +0900

    Update index.js

--- node.js/index.js
+++ node.js/index.js
@@ -38,14 +38,84 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var MongoClient = mongodb.MongoClient;
 
-var url = 'mongodb://localhost:27017'
+var url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, {useNewUrlParser: true}, (err, client)=>{
     if(err)
         console.log('Unable to connect to the mongoDB server.Error', err);
     else{
+
+        app.post('/register', (request, response, next)=>{
+            var post_data = request.body;
+
+            var plaint_passowrd = post_data.password;
+            var hash_data = saltHashPassword(plaint_passowrd);
+
+            var password = hash_data.passwordHash;
+            var salt = hash_data.salt;
+
+            var name = post_data.name;
+            var email = post_data.email;
+
+            var insertJson = {
+                'email': email,
+                'password': password,
+                'salt': salt,
+                'name': name
+            };
+            var db = clinet.db('test');
+
+            db.collection('user')
+                .find({'email': email}).count((err, num)=>{
+                    if(num !=0){
+                        response.json('Email already exists');
+                        console.log('Email already exists');
+                    }
+                    else{
+                        db.collection('user')
+                            .insertOne(insertJson, (error, res)=>{
+                                response.json('Registration succeeded');
+                                console.log('Registration succeeded');
+                            });
+                    }
+                })
+        })
+
+        app.post('/login', (request, response, next)=>{
+            var post_data = request.body;
+            
+            var email = post_data.email;
+            var userPassword = post_data.password;
+
+            var db = clinet.db('test');
+
+            db.collection('user')
+                .find({'email': email}).count((err, num)=>{
+                    if(num ==0){
+                        response.json('Email does not exist');
+                        console.log('Email does not exist');
+                    }
+                    else{
+                        db.collection('user')
+                            .findOne({'email': email}, (error, user)=>{
+                                var salt = user.salt;
+                                var hashed_password = checkHashPassword(userPassword, salt).passwordHash;
+                                var encrypted_password = user.password;
+                                if(hashed_password==encrypted_password){
+                                    response.json('Login succeeded');
+                                    console.log('Login succeeded');
+                                }
+                                else{
+                                    response.json('Wrong password');
+                                    console.log('Wrong password');
+                                }
+                            });
+                    }
+                })
+        });
+
         app.listen(3000, ()=>{
             console.log('Connected to mongoDB server, WebService running on port 3000');
         });
     }
-})
+});
